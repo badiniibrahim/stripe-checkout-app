2d566afec46f0612539aa715998e61bc7d8434b2
Badini Ibrahim <sawadogo.badiniibrahim@gmail.com>
2024-02-19 17:19:23 +0100

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { navLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
+const Home = async () => {
+  return <>Home</>;
+};
+
+export default Home;
